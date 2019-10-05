fa3f2c08d52ceb43f2605e11b9ce6bda1e19f571
AtulDhakad <atuldhakad01@gmail.com>
2019-10-05 16:14:41 +0530

    first145

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var express = require('express');
+const express = require('express');
 const path = require('path');
-
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/myApp'));
+// Serve static files....
+app.use(express.static(__dirname + '/dist/myapp'));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/myApp/index.html'));
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/myapp/index.html'));
 });
 
-app.listen(3000 || process.env.PORT); 
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
