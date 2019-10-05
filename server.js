dbc2fb49e452b02b066141a487da23735a8f602b
AtulDhakad <atuldhakad01@gmail.com>
2019-10-05 16:36:35 +0530

    first145

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/myapp'));
+app.use(express.static(__dirname + '/dist/myApp'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/myapp/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/myApp/index.html'));
 });
 
 // default Heroku PORT
